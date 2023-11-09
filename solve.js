8e81835e137ea650e3d82cd37d9aa444aa3bbd65
yusuf umar faruq <luwaservices@gmail.com>
2023-11-09 10:09:20 -0700

    added

--- solve.js
+++ solve.js
@@ -144,3 +144,47 @@
 //     }
 //     return false;
 //   }
+
+// function getCount(str) {
+//   let vowelsArray = ['a', 'e', 'i', 'o', 'u'];
+//   let vowelCount = 0;
+  
+//   for (let char of str) {
+//     if(vowelsArray.some(el => char === el)) {
+//       vowelCount += 1;
+//     }else {
+//       continue;
+//     }
+//   }
+//   return vowelCount;
+// }
+
+// function findNeedle(haystack) {
+//   // your code here
+//   let index = haystack.indexOf('needle')
+//   if (index !== -1) {
+//     return `found the needle at position ${index}`;
+//   }
+// }
+
+// DESCRIPTION:
+// Input:
+
+// a string strng
+// an array of strings arr
+// Output of function contain_all_rots(strng, arr) (or containAllRots or contain-all-rots):
+
+// a boolean true if all rotations of strng are included in arr
+// false otherwise
+// Examples:
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
+
+// Note:
+// Though not correct in a mathematical sense
+
+// we will consider that there are no rotations of strng == ""
+// and for any array arr: contain_all_rots("", arr) --> true
