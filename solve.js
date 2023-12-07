b1ff457d04bdf3a7e40947f84b072146d337a9b3
yusuf umar faruq <luwaservices@gmail.com>
2023-12-07 14:59:31 -0700

    small enough

--- solve.js
+++ solve.js
@@ -302,3 +302,8 @@
 // console.log(nbYear(1500, 5, 100, 5000))
 // console.log(nbYear(1500000, 2.5, 10000, 2000000))
 // console.log(nbYear(1500000, 0.25, 1000, 2000000))
+
+
+// function smallEnough(a, limit){
+//   return a.every(i => i <= limit);
+// }
