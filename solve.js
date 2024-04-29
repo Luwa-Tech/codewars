74c8f5199163dacab2af6f87d16a5a636a6cb81a
Umar Yusuf <luwaservices@gmail.com>
2024-04-29 23:52:49 +0100

    .

--- solve.js
+++ solve.js
@@ -174,8 +174,6 @@
 // a boolean true if all rotations of strng are included in arr
 // false otherwise
 // Examples:
-// contain_all_rots(
-//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
 
 // contain_all_rots(
 //   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
