6b7b81c82351ed80723ad4639c13b8585913ac27
Umar Yusuf <luwaservices@gmail.com>
2024-05-06 23:45:23 +0100

    .

--- solve.js
+++ solve.js
@@ -24,13 +24,13 @@
 // function bmi(weight, height) {
 //     let BMI = weight / (height*height)
 //     if (BMI <= 18.5) {
-//       return "Underweight"
+//       return "Underweight";
 //     } else if (BMI <= 25.0) {
-//       return "Normal"
+//       return "Normal";
 //     } else if (BMI <= 30.0) {
 //       return "Overweight"
 //     } else {
-//       return "Obese"
+//       return "Obese";
 //     }
 //   }
 
