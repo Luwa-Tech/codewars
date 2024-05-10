967e42e7f6a7b326ba8c683abc9c283a514a209b
Umar Yusuf <luwaservices@gmail.com>
2024-05-10 22:44:18 +0100

    .

--- solve.js
+++ solve.js
@@ -4,22 +4,22 @@
 
 //     let sum = array.reduce((cur, next) => {
 
-//         return cur + next
+//         return cur + next;
 //     })
 
-//     return sum / array.length
+//     return sum / array.length;
 
 // } else {
     
 //     return 0;
 // }
-// }
+// };
 
-// console.log(findAverage([1,1,1]))
-// console.log(findAverage([1,2,3]))
-// console.log(findAverage([1,2,3,4]))
-// console.log(findAverage([1,2,3,4]))
-// console.log(findAverage([1,2,3,4]))
+// console.log(findAverage([1,1,1]));
+// console.log(findAverage([1,2,3]));
+// console.log(findAverage([1,2,3,4]));
+// console.log(findAverage([1,2,3,4]));
+// console.log(findAverage([1,2,3,4]));
 
 // function bmi(weight, height) {
 //     let BMI = weight / (height * height);
@@ -32,8 +32,8 @@
 //       return "Overweight"
 //     } else {
 //       return "Obese";
-//     }
-//   }
+//     };
+//   };
 
 //   console.log(bmi(80, 1.80))
 //   console.log(bmi(80, 1.80))
