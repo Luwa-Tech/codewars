f7dfd8ccbabe7086d17042d57deeb0fbab6857c8
Umar Yusuf <luwaservices@gmail.com>
2024-06-08 22:35:38 +0100

    .

--- solve.js
+++ solve.js
@@ -1,16 +1,11 @@
 // function findAverage(array) {
 //   // your code here
 // if ( array.length > 0 ) {
-
 //     let sum = array.reduce((cur, next) => {
-
 //         return cur + next;
 //     })
-
 //     return sum / array.length;
-
 // } else {
-    
 //     return 0;
 // }
 // };
