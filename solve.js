7c1448ab664589e2ee7a71e6931642dd46038bf6
Umar Yusuf <luwaservices@gmail.com>
2024-06-11 11:15:08 +0100

    .

--- solve.js
+++ solve.js
@@ -64,7 +64,9 @@
 //Given a string of digits, you should replace any digit below 5 //with '0' and any digit 5 and above with '1'. Return the resulting string.
 
 //fakeBin('45385593107843568'), '01011110001100111');
+
 //(fakeBin('509321967506747'), '101000111101101'); 
+
 //(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
 
 // const fakeBin = (stringDigits) => {
