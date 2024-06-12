455164992969cc73516b2a3ebef96b970aebe567
Umar Yusuf <luwaservices@gmail.com>
2024-06-12 23:29:53 +0100

    .

--- solve.js
+++ solve.js
@@ -91,7 +91,6 @@
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
 // Examples:
-
 // Input: 123456789 Output: 987654321
 
 // const rearrange = (nonInteger) => {
