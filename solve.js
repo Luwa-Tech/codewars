5d4b32ecae3581e7b5d1c85f97578476813fd5b7
Umar Yusuf <luwaservices@gmail.com>
2024-06-20 17:37:29 +0100

    .

--- solve.js
+++ solve.js
@@ -164,11 +164,11 @@
 // }
 
 // function findNeedle(haystack) {
-//   let index = haystack.indexOf('needle')
+//   let index = haystack.indexOf('needle');
 //   if (index !== -1) {
 //     return `found the needle at position ${index}`;
 //   }
-// }
+// };
 
 // DESCRIPTION:
 // Input:
