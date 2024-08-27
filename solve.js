352b3546ec3108c474abbc17f863e8cf74d3d248
Umar Yusuf <luwaservices@gmail.com>
2024-08-27 23:26:51 +0100

    .

--- solve.js
+++ solve.js
@@ -159,7 +159,7 @@
 //   return vowelCount;
 // }
 
-// function findNeedle (haystack) {
+// const findNeedle = (haystack) => {
 //   let index = haystack.indexOf('needle');
 //   if (index !== -1) {
 //     return `found the needle at position ${index}`;
