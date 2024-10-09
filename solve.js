42036f272a806b9fc8d3ca67ce50e529d7a453c2
Umar Yusuf <luwaservices@gmail.com>
2024-10-09 17:09:33 +0100

    .

--- solve.js
+++ solve.js
@@ -60,8 +60,6 @@
 
 // console.log(removeExclamationMarks("Hello World!"));
 
-//Given a string of digits, you should replace any digit below 5 //with '0' and any digit 5 and above with '1'. Return the resulting string.
-
 //fakeBin('45385593107843568'), '01011110001100111');
 
 //(fakeBin('509321967506747'), '101000111101101'); 
